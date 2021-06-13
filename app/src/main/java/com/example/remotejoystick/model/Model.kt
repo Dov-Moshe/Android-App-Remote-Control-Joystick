91025c01f1db1b8ac36559ebb1c30e6332c59bba
dov <dovbarak.moshe@live.biu.ac.il>
2021-06-13 12:32:44 +0300

    Update: UI - joystick - first time working in the app.

--- app/src/main/java/com/example/remotejoystick/model/Model.kt
+++ app/src/main/java/com/example/remotejoystick/model/Model.kt
@@ -1,4 +1,117 @@
 package com.example.remotejoystick.model
 
-class Model {
+import android.os.StrictMode
+import android.util.Log
+import android.widget.Toast
+import com.example.remotejoystick.view.MainActivity
+import java.io.DataOutputStream
+import java.lang.Exception
+import java.net.Socket
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.stream.Stream
+
+class Model(activity: MainActivity) : IModel{
+
+    lateinit var socket : Socket
+    lateinit var stream : DataOutputStream
+    var executor : ExecutorService
+    var activity : MainActivity
+
+    init {
+        this.executor = Executors.newFixedThreadPool(1)
+        this.activity = activity
+
+
+        /*try {
+            executor.execute(Runnable { socket = Socket("10.0.0.138",12345) })
+            //val stream = DataOutputStream(socket.getOutputStream())
+            //Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        } catch (e : Exception) {
+            //Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+        }*/
+
+        /*try {
+            var socket = Socket("10.0.0.138",12345)
+            val stream = DataOutputStream(socket.getOutputStream())
+            Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        } catch (e : Exception) {
+            Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+        }*/
+    }
+
+    fun connectToServer(ip : String, port : Int) {
+
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+        try {
+            socket = Socket(ip,port)
+            stream = DataOutputStream(socket.getOutputStream())
+            Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        } catch (e : Exception) {
+            Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+        }
+
+
+        /*if (socket.isConnected) {
+            Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        }*/
+
+        /*Thread(Runnable {
+            fun run() {
+                try  {
+                    socket = Socket("10.0.0.12",12345)
+                    Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+                } catch (e : Exception) {
+                    Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }).start()*/
+
+
+
+
+        /*executor.execute(Runnable { try {
+            socket = Socket("10.0.0.12",12345)
+            //val stream = DataOutputStream(socket.getOutputStream())
+            Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        } catch (e : Exception) {
+            Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+        } })*/
+
+        /*try {
+            socket = Socket("10.0.0.138",12345)
+            //val stream = DataOutputStream(socket.getOutputStream())
+            Toast.makeText(activity, "Connecting to server!", Toast.LENGTH_SHORT).show()
+        } catch (e : Exception) {
+            Toast.makeText(activity, "Cannot connect to server!", Toast.LENGTH_SHORT).show()
+        }*/
+    }
+
+
+
+
+    override fun changeRudder(newVal: Float) {
+        //Log.d("", "Rudder: $newVal")
+        executor.execute(Runnable { try {
+            stream.writeBytes("set /controls/flight/rudder "+newVal+"\r\n")
+            stream.flush()
+        } catch (e : Exception){} })
+    }
+
+    override fun changeThrottle(newVal: Float) {
+        //Log.d("", "Throttle: $newVal")
+        executor.execute(Runnable { try {
+            stream.writeBytes("set /controls/engines/current-engine/throttle "+newVal+"\r\n")
+            stream.flush()
+        } catch (e : Exception){} })
+    }
+
+    override fun changeJoystick(newValFirst: Float, newValSecond: Float) {
+        //Log.d("", "aileron: $newValFirst, elevator: $newValSecond")
+        executor.execute(Runnable { try {
+            stream.writeBytes("set /controls/flight/aileron "+newValFirst+"\r\n")
+            stream.writeBytes("set /controls/flight/elevator "+newValSecond+"\r\n")
+            stream.flush()
+        } catch (e : Exception){} })
+    }
 }
--- app/src/main/java/com/example/remotejoystick/view_model/ViewModel.kt
+++ app/src/main/java/com/example/remotejoystick/view_model/ViewModel.kt
@@ -1,14 +1,49 @@
 package com.example.remotejoystick.view_model
 
+import android.widget.SeekBar
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import com.example.remotejoystick.view.MainActivity
+import com.example.remotejoystick.model.Model
 
-public class ViewModel {
+public class ViewModel(activity: MainActivity) {
 
-    public fun connect(activity: MainActivity, ip : String, port : Int) {
-        //print(ip);
-        //print(port);
-        Toast.makeText(activity, ip, Toast.LENGTH_SHORT).show()
-        Toast.makeText(activity, port.toString(), Toast.LENGTH_SHORT).show()
+    var activity: MainActivity
+    private var model : Model = Model(activity)
+
+    init {
+        this.activity = activity
     }
+
+
+    fun connect(ip : String, port : Int) {
+        this.model.connectToServer(ip,port)
+        //print(ip)
+        //print(port)
+        //Toast.makeText(activity, ip, Toast.LENGTH_SHORT).show()
+        //Toast.makeText(activity, port.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    /*fun setSeekbarProgress(seekBar : SeekBar, newOpacity : Int, fromUser : Boolean) : Unit {
+        Toast.makeText(activity, newOpacity.toString(), Toast.LENGTH_SHORT).show()
+    }*/
+
+    fun setSeekbarProgress(newOpacity : Int, str : String) {
+
+        when(str) {
+            "rudder" -> {
+                val realVal : Float = (newOpacity.toFloat() - 1000) / 1000
+                model.changeRudder(realVal)
+            }
+            "throttle" -> {
+                val realVal : Float = newOpacity.toFloat()/1000
+                model.changeThrottle(realVal)
+            }
+        }
+    }
+
+    fun setJoystickProgress(aileron : Float, elevator : Float) {
+        model.changeJoystick(aileron, elevator)
+    }
+
 }
